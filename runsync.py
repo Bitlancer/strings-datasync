692b083be3c3ca2a6b951c16329079c9cc943624
Edmund Jorgensen <tomheon@gmail.com>
2013-10-11 15:14:49 -0400

    Support common Manager account by adding base dn pre-sync.

--- runsync.py
+++ runsync.py
@@ -1,7 +1,12 @@
 from optparse import OptionParser
+from ConfigParser import SafeConfigParser
 import sys
 
+import ldap
+
 from stringsync.sync import sync_from_config
+from stringsync.mysql2ldif import mysql2ldif, organization_dn
+from stringsync import db
 
 
 def main():
@@ -26,7 +31,23 @@ def main():
 
     with open(db_ini_fname, 'rb') as db_ini_fil:
         with open(ldap_ini_fname, 'rb') as ldap_ini_fil:
-            sync_from_config(db_ini_fil, ldap_ini_fil,
+            db_config_parser = SafeConfigParser()
+            db_config_parser.readfp(db_ini_fil)
+            db_server = db.open_conn(db_config_parser)
+
+            base_dn = organization_dn(organization_id, db_server)
+
+            ldap_config_parser = SafeConfigParser()
+            ldap_config_parser.readfp(ldap_ini_fil)
+            ldap_uri = ldap_config_parser.get('sync', 'uri')
+            ldap_auth_name = ','.join([ldap_config_parser.get('sync', 'name'),
+                                       base_dn])
+            ldap_auth_pass = ldap_config_parser.get('sync', 'pass')
+
+            ldap_server = ldap.initialize(ldap_uri)
+            ldap_server.simple_bind_s(ldap_auth_name, ldap_auth_pass)
+
+            sync_from_config(db_server, ldap_server,
                              organization_id, dry_run=dry_run_fil)
 
 
--- stringsync/sync.py
+++ stringsync/sync.py
@@ -1,8 +1,5 @@
 from StringIO import StringIO
-from ConfigParser import SafeConfigParser
 import sys
-
-import ldap
 
 from stringsync.mysql2ldif import mysql2ldif, organization_dn
 from stringsync.ldif_dumper import dump_tree_sorted
@@ -20,7 +17,6 @@ def _sync_db_to_ldap(organization_id, db_server, ldap_server, dry_run):
 
     new_ldif.seek(0)
 
-
     cur_ldif = StringIO()
     dump_tree_sorted(ldap_server, base_domain, cur_ldif)
     cur_ldif.seek(0)
@@ -31,24 +27,11 @@ def _sync_db_to_ldap(organization_id, db_server, ldap_server, dry_run):
         ldiff_to_ldif(cur_ldif, new_ldif, dry_run)
 
 
-def sync_from_config(db_ini_fil, ldap_ini_fil, organization_id, dry_run=None):
+def sync_from_config(db_server, ldap_server, organization_id, dry_run=None):
     """
     If dry_run is non-None, it is considered a file in which to put
     the ldif, and no changes will be applied to the ldap server
     itself.
     """
-    db_config_parser = SafeConfigParser()
-    db_config_parser.readfp(db_ini_fil)
-    db_server = db.open_conn(db_config_parser)
-
-    ldap_config_parser = SafeConfigParser()
-    ldap_config_parser.readfp(ldap_ini_fil)
-    ldap_uri = ldap_config_parser.get('sync', 'uri')
-    ldap_auth_name = ldap_config_parser.get('sync', 'name')
-    ldap_auth_pass = ldap_config_parser.get('sync', 'pass')
-
-    ldap_server = ldap.initialize(ldap_uri)
-    ldap_server.simple_bind_s(ldap_auth_name, ldap_auth_pass)
-
     _sync_db_to_ldap(organization_id, db_server, ldap_server,
                      dry_run=dry_run)
